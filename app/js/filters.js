e31129c0f9151004ad0c2c3dee167c1e46713f00
Martin Geisler <martin@geisler.net>
2014-08-13 14:35:09 +0200

    Let humanReadable ignore non-numbers
    This will be handy since it lets us give pseudo-directories a
    non-number size (such as '---') which the filter will let pass through
    unchanged.

--- app/js/filters.js
+++ app/js/filters.js
@@ -5,6 +5,9 @@
 angular.module('swiftBrowser.filters', []).
   filter('humanReadable', function() {
       return function(input) {
+          if (!angular.isNumber(input))
+              return input;
+
           var number = parseInt(input);
           var units = ['B', 'KB', 'MB', 'GB'];
           var unit = units.shift();
