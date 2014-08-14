95bb83846b59c2f8e13b70c13c1e7f65cc54bc69
Martin Geisler <martin@geisler.net>
2014-08-14 14:33:53 +0200

    Give byte count units a uniform width
    The new sbFormatBytes directive formats a byte count using the
    humanReadableFilter and wraps the unit in a <span class="unit">
    element.
    
    This element is then given a minimum width to ensure that all units
    take up at least this space (otherwise 'B' take up significantly less
    space than 'KB' or 'MB').

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,4 +7,19 @@ angular.module('swiftBrowser.directives', []).
         return function(scope, elm, attrs) {
             elm.text(version);
         };
+    }]).
+    directive('sbFormatBytes', ['humanReadableFilter', function(humanReadableFilter) {
+        return {
+            restrict: 'AE',
+            scope: {
+                count: '=',
+            },
+            link: function (scope, element, attrs) {
+                var formatted = humanReadableFilter(scope.count);
+                var parts = formatted.split(' ');
+
+                element.text(parts[0] + ' ');
+                element.append($('<span>').addClass('unit').text(parts[1]));
+            },
+        };
     }]);
