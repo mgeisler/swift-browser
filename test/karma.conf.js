283fbe3718bd638d48d11960bd88d27e28bb5889
Martin Geisler <martin@geisler.net>
2014-08-16 19:25:48 +0200

    Add local installation of jQuery
    This is (also) included by Karma when running the test suite. This
    will allow us to test the container controller, which uses jQuery
    internally.
    
    Remember to run "npm install" to actually download jQuery. This runs
    "node_modules/.bin/bower install" for you.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config){
     basePath : '../',
 
     files : [
+      'app/bower_components/jquery/dist/jquery.js',
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
