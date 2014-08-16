76c051f0874c76671a33e8974a8e0e2c0cba3ec8
Martin Geisler <martin@geisler.net>
2014-08-16 18:07:48 +0200

    Refactor common logic for setting up RootCtrl and scope

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,27 +3,30 @@
 /* jasmine specs for controllers go here */
 
 describe('RootCtrl', function(){
+    var scope;
+
     beforeEach(module('swiftBrowser.controllers'));
 
-    it('should list containers', inject(function($controller, $httpBackend) {
+    beforeEach(inject(function($controller) {
+        scope = {};
+        $controller('RootCtrl', {$scope: scope});
+    }));
+
+    it('should list containers', inject(function($httpBackend) {
         var containers = [
             {"count": 10, "bytes": 1234, "name": "foo"},
             {"count": 20, "bytes": 2345, "name": "bar"},
         ];
-        var scope = {};
         $httpBackend.whenGET('/v1/AUTH_abc?format=json')
             .respond(200, containers);
 
-        $controller('RootCtrl', {$scope: scope});
         expect(scope.containers).toEqual([]);
         $httpBackend.flush();
         expect(scope.containers).toEqual(containers);
     }));
 
-    it('should set sort order', inject(function($controller) {
-        var scope = {};
-        $controller('RootCtrl', {$scope: scope});
+    it('should set sort order', function() {
         expect(scope.orderProp).toEqual('name');
-    }));
+    });
 
 });
