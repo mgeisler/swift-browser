5fcb185643fe1ccddaa5249cc17eb7efc13a37b1
Martin Geisler <martin@geisler.net>
2014-08-18 15:10:32 +0200

    e2e: add end-to-end test for root page
    This works by injecting JavaScript code into the running browser to
    configure the $httpBackend service.
    
    This is done using the addMockModule protractor method which installs
    code to run *before* Angular has been loaded. The code is run before
    every browser.get call.
    
    The page much include angular-mocks.js for this to work. This script
    is added to the page before each test is run. If multiple tests are
    run the file would be added multiple times since the browser doesn't
    do a full page refresh between the tests.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -9,3 +9,49 @@ describe('my app', function() {
   });
 
 });
+
+
+describe('Container listing', function () {
+
+    beforeEach(function () {
+        browser.addMockModule('swiftBrowserE2E', function () {
+            if (!window.e2e_angular_mocks_loaded) {
+                var script = document.createElement('script');
+                script.src = 'bower_components/angular-mocks/angular-mocks.js';
+                document.body.appendChild(script);
+                window.e2e_angular_mocks_loaded = true;
+            }
+            angular.module('swiftBrowserE2E', ['ngMockE2E']);
+        });
+    });
+
+    it('should be sortable', function () {
+        browser.addMockModule('swiftBrowserE2E', function () {
+            angular.module('swiftBrowserE2E').run(function($httpBackend) {
+                $httpBackend.whenGET('/app/index.html?format=json').respond([
+                        {name: "bar", count: 20, bytes: 1234},
+                        {name: "foo", count: 10, bytes: 2345}
+                    ]);
+                    $httpBackend.whenGET(/.*/).passThrough();
+            });
+        });
+
+        function mapGetText(locator) {
+            return element.all(locator).map(function (el) {
+                return el.getText()
+            });
+        }
+
+        browser.get('index.html#/');
+        var rows = by.repeater('container in containers');
+        var names = rows.column('{{ container.name }}');
+
+        // Initial sort order is by name
+        expect(mapGetText(names)).toEqual(['bar', 'foo']);
+
+        // Clicking the name header sorts reverses the order
+        element(by.css('th')).click();
+        expect(mapGetText(names)).toEqual(['foo', 'bar']);
+    });
+
+});
