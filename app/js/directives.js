0085f69e40ed8a1fb6cc3743dc2752d02c3c4a94
Martin Geisler <martin@geisler.net>
2014-08-18 08:51:23 +0200

    Simplify sbFormatBytes by using template over jQuery

--- app/js/directives.js
+++ app/js/directives.js
@@ -17,9 +17,9 @@ angular.module('swiftBrowser.directives', []).
             link: function (scope, element, attrs) {
                 var formatted = bytesFilter(scope.count);
                 var parts = formatted.split(' ');
-
-                element.text(parts[0] + ' ');
-                element.append($('<span>').addClass('unit').text(parts[1]));
+                scope.number = parts[0];
+                scope.unit = parts[1];
             },
+            template: '{{number}} <span class="unit">{{unit}}</span>',
         };
     }]);
