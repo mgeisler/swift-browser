fb30437274012693796d8266bd9b70702ff882ae
Martin Geisler <martin@geisler.net>
2014-08-18 16:34:35 +0200

    e2e: test sorting container listing by count and size
    For the size column, we cannot use the by.repeater(...) helper locator
    since the column method doesn't work when there's no content to match
    on. We select the column by CSS instead then -- this will of course
    break if the columns are ever reordered, but the breakage will be easy
    fix then.

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -25,33 +25,65 @@ describe('Container listing', function () {
         });
     });
 
-    it('should be sortable', function () {
-        browser.addMockModule('swiftBrowserE2E', function () {
-            angular.module('swiftBrowserE2E').run(function($httpBackend) {
-                $httpBackend.whenGET('/app/index.html?format=json').respond([
-                        {name: "bar", count: 20, bytes: 1234},
-                        {name: "foo", count: 10, bytes: 2345}
-                    ]);
-                    $httpBackend.whenGET(/.*/).passThrough();
-            });
-        });
-
+    describe('should be sortable', function () {
         function mapGetText(locator) {
             return element.all(locator).map(function (el) {
                 return el.getText()
             });
         }
 
-        browser.get('index.html#/');
-        var rows = by.repeater('container in containers');
-        var names = rows.column('{{ container.name }}');
+        beforeEach(function () {
+            browser.addMockModule('swiftBrowserE2E', function () {
+                angular.module('swiftBrowserE2E').run(function($httpBackend) {
+                    $httpBackend.whenGET('/app/index.html?format=json').respond([
+                        {name: "bar", count: 20, bytes: 1234},
+                        {name: "foo", count: 10, bytes: 2345}
+                    ]);
+                    $httpBackend.whenGET(/.*/).passThrough();
+                });
+            });
 
-        // Initial sort order is by name
-        expect(mapGetText(names)).toEqual(['bar', 'foo']);
+            browser.get('index.html#/');
+        });
 
-        // Clicking the name header sorts reverses the order
-        element(by.css('th')).click();
-        expect(mapGetText(names)).toEqual(['foo', 'bar']);
+        it('by name', function () {
+            var rows = by.repeater('container in containers');
+            var names = rows.column('{{ container.name }}');
+
+            // Initial sort order is by name
+            expect(mapGetText(names)).toEqual(['bar', 'foo']);
+            // Clicking the name header sorts reverses the order
+            element(by.css('th')).click();
+            expect(mapGetText(names)).toEqual(['foo', 'bar']);
+        });
+
+        it('by size', function () {
+            var sizes = by.css('td:nth-child(2)');
+
+            // Initial sort
+            expect(mapGetText(sizes)).toEqual(['1.2 KB', '2.3 KB']);
+            // Sorting by size makes no change
+            element.all(by.css('th')).get(1).click();
+            expect(mapGetText(sizes)).toEqual(['1.2 KB', '2.3 KB']);
+            // Clicking again reverses
+            element.all(by.css('th')).get(1).click();
+            expect(mapGetText(sizes)).toEqual(['2.3 KB', '1.2 KB']);
+        });
+
+        it('by count', function () {
+            var rows = by.repeater('container in containers');
+            var counts = rows.column('{{ container.count | number }}');
+
+            // Initial sort order is by name
+            expect(mapGetText(counts)).toEqual(['20 objects', '10 objects']);
+            // Clicking the header sorts
+            element.all(by.css('th')).get(2).click();
+            expect(mapGetText(counts)).toEqual(['10 objects', '20 objects']);
+            // Clicking the header sorts reverses the order
+            element.all(by.css('th')).get(2).click();
+            expect(mapGetText(counts)).toEqual(['20 objects', '10 objects']);
+        });
+
     });
 
 });
