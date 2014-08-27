1b7f2a019c48ccb4f5591dac47030fa16119ab2b
Martin Geisler <martin@geisler.net>
2014-08-27 16:45:31 +0200

    swift-simulator: rename load guard to camel case name

--- test/swift-simulator.js
+++ test/swift-simulator.js
@@ -1,11 +1,11 @@
 
 exports.loadAngularMocks = function () {
     browser.addMockModule('swiftBrowserE2E', function () {
-        if (!window.e2e_angular_mocks_loaded) {
+        if (!window.e2eAngularMocksLoaded) {
             var script = document.createElement('script');
             script.src = 'bower_components/angular-mocks/angular-mocks.js';
             document.body.appendChild(script);
-            window.e2e_angular_mocks_loaded = true;
+            window.e2eAngularMocksLoaded = true;
         }
         angular.module('swiftBrowserE2E', ['ngMockE2E']);
     });
