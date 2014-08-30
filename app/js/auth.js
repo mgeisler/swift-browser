cd8e7b0ab7120e9ef4d8bb41fa7bce7793d07e98
Martin Geisler <martin@geisler.net>
2014-08-30 15:56:49 +0200

    auth: add some documentation

--- app/js/auth.js
+++ app/js/auth.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/* Service for tracking authentication status. The `state` property
+ * shows the currently known status.
+ *
+ * Initial status is 'auth-done'.
+ */
 function AuthService($q, $swift, $http) {
     this.$q = $q;
     this.$swift = $swift;
@@ -9,6 +14,12 @@ function AuthService($q, $swift, $http) {
     this.configs = [];
 }
 
+/* Request authentication for the HTTP request with configuration
+ * `config`. The configuration will be used later to resend the
+ * request when `authenticate` is called.
+ *
+ * Changes state to "auth-request".
+ */
 AuthService.prototype.requestAuth = function (config) {
     var result = this.$q.defer();
     this.deferreds.push(result);
@@ -17,6 +28,11 @@ AuthService.prototype.requestAuth = function (config) {
     return result.promise;
 };
 
+/* Authenticate to Swift and resolve pending HTTP requests.
+ *
+ * Changes state to 'auth-started' immediately and to 'auth-done' when
+ * successfully authenticated.
+ */
 AuthService.prototype.authenticate = function(credentials) {
     this.state = 'auth-started';
     var that = this;
