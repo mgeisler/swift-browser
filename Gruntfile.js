984b12c75c93bc35c7e7349b2fc5f159808aca2f
Martin Geisler <martin@geisler.net>
2014-09-18 16:43:14 +0200

    grunt: add coverage task (fixes #24)
    This task is a combination of four other tasks:
    
    * copy from grunt-contrib-copy: copies app/ to coverage/instrumented,
      with some unneeded files excluded
    
    * instrument from grunt-istanbul: adds instrumentation to our
      JavaScript files. The instrumented files are put into
      coverage/instrumented/
    
    * protractor_coverage from grunt-protractor-coverage: runs protractor
      on the instrumented files. This task starts a coverage-collector
      server running on port 3001. The tests are modified on the fly to
    
      1. Ask the browser for the __coverage__ object which is where the
         instrumented files record the coverage data.
    
      2. POST this __coverage__ object to http://localhost:3001
    
      Protractor is done, the total coverage data is collected from the
      coverage collector and stored in coverage/coverage.json.
    
      The specs glob is listed here again since protractor_coverage
      handles it differently than protractor itself: protractor expands
      the globs relative to the config file and protractor_coverage
      expands them relative to the current working directory.
    
    * makeReport from grunt-istanbul: outputs a coverage report to the
      console and a detailed HTML in coverage/e2e/lcov-report.

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,15 +46,56 @@ module.exports = function(grunt) {
             target: {
                 src: 'coverage/lcov.info'
             }
-        }
+        },
+
+        copy: {
+            e2e: {
+                src: ['app/**/*', '!**/*.orig', '!**/*~'],
+                dest: 'coverage/instrumented/'
+            },
+        },
+        instrument: {
+            files: 'app/js/**/*.js',
+            options: {
+                basePath: "coverage/instrumented"
+            }
+        },
+        protractor_coverage: {
+            e2e: {
+                options: {
+                    coverageDir: 'coverage',
+                    args: {
+                        baseUrl: 'http://localhost:8000/coverage/instrumented/app/',
+                        specs: ['test/e2e/*.js']
+                    },
+                    configFile: 'test/protractor-conf.js',
+                }
+            }
+        },
+        makeReport: {
+            src: 'coverage/coverage.json',
+            options: {
+                type: 'lcov',
+                dir: 'coverage/e2e',
+                print: 'detail'
+            }
+        },
+
     });
 
     grunt.registerTask('update-webdriver', ['exec:webdriver']);
     grunt.registerTask('start', ['connect:server']);
+    grunt.registerTask('coverage', [
+        'copy', 'instrument', 'protractor_coverage', 'makeReport'
+    ]);
+
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-eslint');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-protractor-runner');
     grunt.loadNpmTasks('grunt-exec');
     grunt.loadNpmTasks('grunt-coveralls');
+    grunt.loadNpmTasks('grunt-protractor-coverage');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-istanbul');
 };
