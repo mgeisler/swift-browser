fc38ed643f2472df94fad7eac7cf9518bf4d0c68
Martin Geisler <martin@geisler.net>
2014-09-25 14:59:14 +0200

    swift-simulator: move code out of swift-mock (fixes #59)
    This makes it possible to debug the code in the simulator: the code is
    loaded using a script element during testing and this makes it
    possible to set breakpoints like normal when the browser is paused
    with a call to browser.pause() in a test.

--- test/swift-mock.js
+++ test/swift-mock.js
@@ -3,9 +3,13 @@
 exports.loadAngularMocks = function () {
     browser.addMockModule('swiftBrowserE2E', function () {
         if (!window.e2eAngularMocksLoaded) {
-            var script = document.createElement('script');
-            script.src = 'bower_components/angular-mocks/angular-mocks.js';
-            document.body.appendChild(script);
+            var ngMocks = document.createElement('script');
+            ngMocks.src = 'bower_components/angular-mocks/angular-mocks.js';
+            document.body.appendChild(ngMocks);
+            var swiftSim = document.createElement('script');
+            swiftSim.src = 'js/test/swift-simulator.js';
+            document.body.appendChild(swiftSim);
+
             window.e2eAngularMocksLoaded = true;
         }
         angular.module('swiftBrowserE2E', ['ngMockE2E']);
@@ -13,11 +17,7 @@ exports.loadAngularMocks = function () {
 };
 
 exports.commit = function () {
-    browser.addMockModule('swiftBrowserE2E', function () {
-        angular.module('swiftBrowserE2E').run(function($httpBackend) {
-            $httpBackend.whenGET(/.*/).passThrough();
-        });
-    });
+    browser.addMockModule('swiftBrowserE2E', 'window.commit()');
 };
 
 exports.setContainers = function(containers) {
@@ -34,79 +34,14 @@ exports.setContainers = function(containers) {
        properties are no longer defined. Converting to and from JSON
        is a work-around for this.
     */
-    browser.addMockModule('swiftBrowserE2E', function () {
-        var containers = JSON.parse(arguments[0]);
-        angular.module('swiftBrowserE2E').run(function($httpBackend) {
-            var path = window.location.pathname;
-            var accountUrl = path.split('/').slice(0, 3).join('/');
-
-            $httpBackend.whenGET(accountUrl + '?format=json')
-                .respond(containers);
-        });
-    }, JSON.stringify(containers));
+    browser.addMockModule('swiftBrowserE2E',
+                          'window.setContainers(arguments[0])',
+                          JSON.stringify(containers));
 };
 
 exports.setObjects = function(container, objects) {
-    browser.addMockModule('swiftBrowserE2E', function () {
-        var container = arguments[0];
-        var objects = JSON.parse(arguments[1]);
+    browser.addMockModule('swiftBrowserE2E',
+                          'window.setObjects(arguments[0], arguments[1])',
+                          container, JSON.stringify(objects));
 
-        function escape(string) {
-            return string.replace(/([.*+?^${}()|\[\]\/\\])/g, "\\$1");
-        }
-
-        function parseQueryString(qs) {
-            var params = {};
-            var parts = qs.split('&');
-            for (var i = 0; i < parts.length; i++) {
-                var keyvalue = parts[i].split('=');
-                var key = decodeURIComponent(keyvalue[0]);
-                var value = decodeURIComponent(keyvalue[1]);
-                params[key] = value;
-            }
-            return params;
-        }
-
-        var path = window.location.pathname;
-        var accountUrl = path.split('/').slice(0, 3).join('/');
-        var fixed = accountUrl + '/' + container;
-        var listRegex = new RegExp(escape(fixed + '?') + '(.*)');
-        var deleteRegex = new RegExp(escape(fixed + '/') + '(.*)');
-
-        function listObjects(method, url, data) {
-            var match = url.match(listRegex);
-            var params = parseQueryString(match[1]);
-            var results = [];
-            for (var i = 0; i < objects.length; i++) {
-                var object = objects[i];
-                if (object.name.indexOf(params.prefix) == 0) {
-                    var rest = object.name.slice(params.prefix.length);
-                    var idx = rest.indexOf(params.delimiter);
-                    if (idx > -1) {
-                        results.push({subdir: rest.slice(0, idx + 1)});
-                    } else {
-                        results.push(object);
-                    }
-                }
-            }
-            return [200, results];
-        }
-
-        function deleteObject(method, url, data) {
-            var match = url.match(deleteRegex);
-            var name = match[1];
-            for (var i = 0; i < objects.length; i++) {
-                if (objects[i].name == name) {
-                    objects.splice(i, 1);
-                    return [204, null];
-                }
-            }
-            return [404, 'Not Found'];
-        }
-
-        angular.module('swiftBrowserE2E').run(function($httpBackend) {
-            $httpBackend.whenGET(listRegex).respond(listObjects);
-            $httpBackend.whenDELETE(deleteRegex).respond(deleteObject);
-        });
-    }, container, JSON.stringify(objects));
 };
