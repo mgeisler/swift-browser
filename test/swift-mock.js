fd55e95cd6ed057fec53e4829748aee4962c809d
Martin Geisler <martin@geisler.net>
2014-10-21 10:55:19 +0200

    swift-mock: refactor script loading

--- test/swift-mock.js
+++ test/swift-mock.js
@@ -3,13 +3,15 @@
 exports.loadAngularMocks = function () {
     browser.clearMockModules();
     browser.addMockModule('swiftBrowserE2E', function () {
-        var ngMocks = document.createElement('script');
-        ngMocks.src = 'bower_components/angular-mocks/angular-mocks.js';
-        document.body.appendChild(ngMocks);
-
-        var swiftSim = document.createElement('script');
-        swiftSim.src = 'js/test/swift-simulator.js';
-        document.body.appendChild(swiftSim);
+        var srcs = [
+            'bower_components/angular-mocks/angular-mocks.js',
+            'js/test/swift-simulator.js'
+        ];
+        srcs.forEach(function (src) {
+            var script = document.createElement('script');
+            script.src = src;
+            document.body.appendChild(script);
+        });
     });
     browser.addMockModule('swiftBrowserE2E', function() {
         angular.module('swiftBrowserE2E').run(function(swiftSim) {
