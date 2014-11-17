3a9c5e7ce6c40864551dd62394f256fa169f22e2
Martin Geisler <martin@geisler.net>
2014-11-17 11:02:18 +0100

    package: bump version to 0.2.0

--- app/js/services.js
+++ app/js/services.js
@@ -3,4 +3,4 @@
 /* Services */
 
 angular.module('swiftBrowser.services', []).
-    value('version', '0.1.0');
+    value('version', '0.2.0');
--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -7,7 +7,7 @@ describe('service', function () {
 
   describe('version', function () {
     it('should return current version', inject(function (version) {
-      expect(version).toEqual('0.1.0');
+      expect(version).toEqual('0.2.0');
     }));
   });
 });
