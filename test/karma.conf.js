d23a8459f0b51cc3d1fd5914822acb692c544595
Martin Geisler <martin@geisler.net>
2014-11-19 14:10:18 +0100

    karma: indent like our other files

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -3,41 +3,41 @@
 var path = require('path');
 
 module.exports = function (config) {
-  function prefix(str, files) {
-    return files.map(function (f) {
-      return path.join(str, f);
-    });
-  }
+    function prefix(str, files) {
+        return files.map(function (f) {
+            return path.join(str, f);
+        });
+    }
 
-  config.set({
-    basePath: '../',
-    files: prefix('app/bower_components', [
-      'codemirror/lib/codemirror.js',
-      'codemirror/mode/meta.js',
-      'codemirror/addon/mode/loadmode.js',
-      'ng-file-upload/angular-file-upload-html5-shim.js',
-      'angular/angular.js',
-      'ng-file-upload/angular-file-upload.js',
-      'angular-ui-router/release/angular-ui-router.js',
-      'angular-ui-codemirror/ui-codemirror.js',
-      'angular-mocks/angular-mocks.js',
-      'angular-bootstrap/ui-bootstrap-tpls.js',
-    ]).concat([
-      'app/js/*.js',
-      'test/unit/**/*.js'
-    ]),
-    // Files created by Emacs FlyCheck
-    exclude: ['**/flycheck_*.js'],
-    autoWatch: true,
-    frameworks: ['jasmine'],
-    browsers: ['Chrome', 'Firefox'],
-    urlRoot: '/v1/AUTH_abc/container/',
-    plugins: [
+    config.set({
+        basePath: '../',
+        files: prefix('app/bower_components', [
+            'codemirror/lib/codemirror.js',
+            'codemirror/mode/meta.js',
+            'codemirror/addon/mode/loadmode.js',
+            'ng-file-upload/angular-file-upload-html5-shim.js',
+            'angular/angular.js',
+            'ng-file-upload/angular-file-upload.js',
+            'angular-ui-router/release/angular-ui-router.js',
+            'angular-ui-codemirror/ui-codemirror.js',
+            'angular-mocks/angular-mocks.js',
+            'angular-bootstrap/ui-bootstrap-tpls.js',
+        ]).concat([
+            'app/js/*.js',
+            'test/unit/**/*.js'
+        ]),
+        // Files created by Emacs FlyCheck
+        exclude: ['**/flycheck_*.js'],
+        autoWatch: true,
+        frameworks: ['jasmine'],
+        browsers: ['Chrome', 'Firefox'],
+        urlRoot: '/v1/AUTH_abc/container/',
+        plugins: [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            ],
+        ],
 
-    reporters: ['progress'],
-  });
+        reporters: ['progress'],
+    });
 };
