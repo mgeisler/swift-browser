1c606a90aed895b7e330edf19f1f421ed8636548
Martin Geisler <martin@geisler.net>
2015-10-04 16:29:28 +0200

    directives: fix indentation in unit test

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -3,17 +3,17 @@
 /* jasmine specs for directives go here */
 
 describe('directives', function () {
-  beforeEach(module('swiftBrowser.directives'));
+    beforeEach(module('swiftBrowser.directives'));
 
-  describe('sb-version', function () {
-    it('should print current version', function () {
-      module(function ($provide) {
-        $provide.value('version', '0.123');
-      });
-      inject(function ($compile, $rootScope) {
-        var element = $compile('<span sb-version></span>')($rootScope);
-        expect(element.text()).toEqual('0.123');
-      });
+    describe('sb-version', function () {
+        it('should print current version', function () {
+            module(function ($provide) {
+                $provide.value('version', '0.123');
+            });
+            inject(function ($compile, $rootScope) {
+                var element = $compile('<span sb-version></span>')($rootScope);
+                expect(element.text()).toEqual('0.123');
+            });
+        });
     });
-  });
 });
