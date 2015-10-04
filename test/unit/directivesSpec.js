1454bd7a52e8ccf5aff310f1f0879ae14b20b679
Martin Geisler <martin@geisler.net>
2015-10-04 18:54:39 +0200

    breadcrumbs: test new directive

--- test/unit/directivesSpec.js
+++ test/unit/directivesSpec.js
@@ -16,4 +16,42 @@ describe('directives', function () {
             });
         });
     });
+
+    describe('sb-breadcrumbs', function () {
+        beforeEach(module('partials/breadcrumbs.html'));
+
+        beforeEach(inject(function ($compile, $rootScope) {
+            this.$compile = $compile;
+            this.$rootScope = $rootScope;
+        }));
+
+        function getCrumbs(path) {
+            var html = ('<sb-breadcrumbs path="' + path + '">' +
+                        '</sb-breadcrumbs>');
+            var element = this.$compile(html)(this.$rootScope);
+            this.$rootScope.$digest();
+            var scope = element.isolateScope();
+            // Strip $$hashKey fields
+            return angular.copy(scope.crumbs);
+        }
+
+        var home = {path: '/', icon: 'glyphicon glyphicon-home'};
+        var blah = {path: '/blah/', title: 'blah'};
+        var atxt = {path: '/blah/a.txt', title: 'a.txt'};
+
+        it('should handle root', function () {
+            var crumbs = getCrumbs.call(this, '/');
+            expect(crumbs).toEqual([home]);
+        });
+
+        it('should handle container', function () {
+            var crumbs = getCrumbs.call(this, '/blah/');
+            expect(crumbs).toEqual([home, blah]);
+        });
+
+        it('should handle objects', function () {
+            var crumbs = getCrumbs.call(this, '/blah/a.txt');
+            expect(crumbs).toEqual([home, blah, atxt]);
+        });
+    });
 });
